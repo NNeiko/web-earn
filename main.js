45ad8da597e007d57b3eb9d08db984b431121af3
Alfariz <aldialfrz100@gmail.com>
2024-10-02 13:07:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('Hello World!');
+
+
+//confirm('apakah anda pria sigma?');//
+
+
+let hewan = 'ikan, gajah, ayam';
+console.log(hewan)
+
+
+var y = 1;
+var x = 8;
+var z = y + x;
+console.log(z)
+
+
+document.getElementById("demo")
+.innerHTML = "thanks";
